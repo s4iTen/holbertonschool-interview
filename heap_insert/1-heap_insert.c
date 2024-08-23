d8b96f9fc85216045d456db3db0e00eabdb394a2
s4iTen <ychouk102@gmail.com>
2024-08-23 18:08:00 +0200

    this is the last updates

--- heap_insert/1-heap_insert.c
+++ heap_insert/1-heap_insert.c
@@ -1,53 +1,101 @@
 #include "binary_trees.h"
 
 /**
- * heap_insert - Inserts a value into a Max Binary Heap.
- * @root: Double pointer to the root node of the Heap.
- * @value: The value to store in the node to be inserted.
- * Return: Pointer to the inserted node, or NULL on failure.
+ * binary_tree_size - Measures the size of a binary tree
+ * @tree: Pointer to the root node of the tree to measure
+ *
+ * Return: Size of the tree
  */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+    if (tree == NULL)
+        return (0);
+    return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
+}
 
+/**
+ * binary_tree_is_perfect - Checks if a binary tree is perfect
+ * @tree: Pointer to the root node of the tree to check
+ *
+ * Return: 1 if the tree is perfect, 0 otherwise
+ */
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+    if (tree == NULL)
+        return (0);
+    if (binary_tree_size(tree->left) == binary_tree_size(tree->right))
+        return (1);
+    return (0);
+}
+
+/**
+ * heapify_up - Bubbles up the node to maintain the max-heap property
+ * @node: Pointer to the node to heapify up
+ *
+ * Return: Pointer to the node after heapifying
+ */
+heap_t *heapify_up(heap_t *node)
+{
+    int temp;
+
+    while (node->parent && node->n > node->parent->n)
+    {
+        temp = node->n;
+        node->n = node->parent->n;
+        node->parent->n = temp;
+        node = node->parent;
+    }
+    return (node);
+}
+
+/**
+ * heap_insert - Inserts a value into a Max Binary Heap
+ * @root: Double pointer to the root node of the Heap
+ * @value: Value to store in the node to be inserted
+ *
+ * Return: Pointer to the inserted node, or NULL on failure
+ */
 heap_t *heap_insert(heap_t **root, int value)
 {
-    heap_t *new_node, *current;
+    heap_t *new_node, *parent;
+    size_t size;
+    unsigned int bit;
 
-    new_node = binary_tree_node(NULL, value);
-    if (!new_node)
+    if (root == NULL)
         return (NULL);
 
-    if (!*root)
-        return (*root = new_node);
-
-    current = *root;
-    while (current->left || current->right)
+    if (*root == NULL)
     {
-        if (value <= current->n)
-            break;
+        *root = binary_tree_node(NULL, value);
+        return (*root);
+    }
 
-        if (!current->right || current->left->n > current->right->n)
-        {
-            current = current->left;
-        }
+    size = binary_tree_size(*root) + 1;
+    bit = 1;
+
+    while (size >> bit)
+        bit++;
+    bit -= 2;
+
+    parent = *root;
+    while (bit)
+    {
+        if (size >> bit & 1)
+            parent = parent->right;
         else
-        {
-            current = current->right;
-        }
+            parent = parent->left;
+        bit--;
     }
 
-    new_node->parent = current;
-    if (!current->left)
-        current->left = new_node;
+    if (size & 1)
+        parent->right = binary_tree_node(parent, value);
     else
-        current->right = new_node;
+        parent->left = binary_tree_node(parent, value);
 
-    while (new_node->parent && new_node->n > new_node->parent->n)
-    {
-        /*combines two values using bitwise and assigns the result back to the left operand.*/
-        new_node->n ^= new_node->parent->n;
-        new_node->parent->n ^= new_node->n;
-        new_node->n ^= new_node->parent->n;
-        new_node = new_node->parent;
-    }
+    if (size & 1)
+        new_node = parent->right;
+    else
+        new_node = parent->left;
 
-    return (new_node);
+    return (heapify_up(new_node));
 }
