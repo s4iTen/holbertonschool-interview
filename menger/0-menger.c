ed544a113fd7f794d7d7ff38d1a2a697fea939cd
s4iTen <ychouk102@gmail.com>
2024-08-25 18:34:25 +0200

    this is the last upadtes

--- menger/0-menger.c
+++ menger/0-menger.c
@@ -1,22 +1,28 @@
 #include <stdio.h>
 #include <math.h>
 
-void menger(int level) {
+void menger(int level)
+{
 	if (level < 0)
 		return;
 
-	int size = pow(3, level);  // Calculate the size of the sponge
-	for (int y = 0; y < size; y++) {
-		for (int x = 0; x < size; x++) {
-			int draw = 1;  // Initially assume we'll draw a #
+	int size = pow(3, level);
+
+	for (int y = 0; y < size; y++)
+	{
+		for (int x = 0; x < size; x++)
+		{
+			int draw = 1;
 
 			int x_temp = x;
 			int y_temp = y;
 
-			// Check the coordinates to see if it falls in a hole
-			while (x_temp > 0 || y_temp > 0) {
-				if (x_temp % 3 == 1 && y_temp % 3 == 1) {
-					draw = 0;  // Mark as a hole
+
+			while (x_temp > 0 || y_temp > 0)
+			{
+				if (x_temp % 3 == 1 && y_temp % 3 == 1)
+				{
+					draw = 0;
 					break;
 				}
 				x_temp /= 3;
