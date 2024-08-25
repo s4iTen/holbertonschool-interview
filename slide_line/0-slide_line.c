82eb1c6db799595c5c959f8dfcb72260018aa821
s4iTen <ychouk102@gmail.com>
2024-08-25 18:21:39 +0200

    this is the last updates

--- slide_line/0-slide_line.c
+++ slide_line/0-slide_line.c
@@ -1,92 +1,50 @@
 #include "slide_line.h"
 
-/**
- * merge_values - Merge contiguous equal values in an array based on direction
- * @line: Pointer to the array of integers
- * @size: Size of the array
- * @direction: SLIDE_LEFT or SLIDE_RIGHT
- */
-void merge_values(int *line, size_t size, int direction)
-{
-	size_t i, j;
+void slide_left(int *line, size_t size) {
+    size_t i, pos = 0;
 
-	if (direction == SLIDE_LEFT)
-	{
-		for (i = 0; i < size - 1; i++)
-		{
-			if (line[i] == line[i + 1])
-			{
-				line[i] *= 2;
-				line[i + 1] = 0;
-			}
-		}
-	}
-	else if (direction == SLIDE_RIGHT)
-	{
-		for (j = size - 1; j > 0; j--)
-		{
-			if (line[j] == line[j - 1])
-			{
-				line[j] *= 2;
-				line[j - 1] = 0;
-			}
-		}
-	}
+    for (i = 0; i < size; i++) {
+        if (line[i] != 0) {
+            if (pos > 0 && line[pos - 1] == line[i]) {
+                line[pos - 1] *= 2;
+                line[i] = 0;
+            } else {
+                if (i != pos) {
+                    line[pos] = line[i];
+                    line[i] = 0;
+                }
+                pos++;
+            }
+        }
+    }
 }
 
-/**
- * shift_values - Shift non-zero values to the specified direction
- * @line: Pointer to the array of integers
- * @size: Size of the array
- * @direction: SLIDE_LEFT or SLIDE_RIGHT
- */
-void shift_values(int *line, size_t size, int direction)
-{
-	size_t i, j;
-	int temp;
+void slide_right(int *line, size_t size) {
+    ssize_t i, pos = size - 1;
 
-	if (direction == SLIDE_LEFT)
-	{
-		for (i = 0, j = 0; i < size; i++)
-		{
-			if (line[i] != 0)
-			{
-				temp = line[i];
-				line[i] = 0;
-				line[j++] = temp;
-			}
-		}
-	}
-	else if (direction == SLIDE_RIGHT)
-	{
-		for (i = size; i > 0; i--)
-		{
-			if (line[i - 1] != 0)
-			{
-				temp = line[i - 1];
-				line[i - 1] = 0;
-				line[--j] = temp;
-			}
-		}
-	}
+    for (i = size - 1; i >= 0; i--) {
+        if (line[i] != 0) {
+            if (pos < size - 1 && line[pos + 1] == line[i]) {
+                line[pos + 1] *= 2;
+                line[i] = 0;
+            } else {
+                if (i != pos) {
+                    line[pos] = line[i];
+                    line[i] = 0;
+                }
+                pos--;
+            }
+        }
+    }
 }
 
-
-/**
- * slide_line - Slide and merge an array of integers to the specified direction
- * @line: Pointer to the array of integers
- * @size: Size of the array
- * @direction: SLIDE_LEFT or SLIDE_RIGHT
- * Return: 1 if successful, 0 if failed
- */
-int slide_line(int *line, size_t size, int direction)
-{
-	if (direction != SLIDE_LEFT && direction != SLIDE_RIGHT)
-		return (0);
-
-	shift_values(line, size, direction);
-	merge_values(line, size, direction);
-	shift_values(line, size, direction);
-
-	return (1);
+int slide_line(int *line, size_t size, int direction) {
+    if (direction == SLIDE_LEFT) {
+        slide_left(line, size);
+    } else if (direction == SLIDE_RIGHT) {
+        slide_right(line, size);
+    } else {
+        return 0;
+    }
+    return 1;
 }
