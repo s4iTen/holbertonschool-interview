e66c05ffbb7d238cea68a83c1f8f84ac74a89a55
s4iTen <ychouk102@gmail.com>
2024-08-28 01:13:43 +0200

    this is the changement of the multiply function

--- infinite_multiplication/0-mul.c
+++ infinite_multiplication/0-mul.c
@@ -45,42 +45,44 @@ int _strlen(char *s)
  */
 void multiply(char *num1, char *num2)
 {
-	int len1 = _strlen(num1);
-	int len2 = _strlen(num2);
-	int *result = calloc(len1 + len2, sizeof(int));
-	int i, j, carry, n1, n2, sum;
+    int len1 = _strlen(num1);
+    int len2 = _strlen(num2);
+    int *result = calloc(len1 + len2, sizeof(int));
+    int i, j, carry, n1, n2, sum;
 
-	if (!result)
-		exit(98);
+    if (!result)
+        exit(98);
 
-	for (i = len1 - 1; i >= 0; i--)
-	{
-		n1 = num1[i] - '0';
-		carry = 0;
-		for (j = len2 - 1; j >= 0; j--)
-		{
-			n2 = num2[j] - '0';
-			sum = n1 * n2 + result[i + j + 1] + carry;
-			carry = sum / 10;
-			result[i + j + 1] = sum % 10;
-		}
-		result[i + j + 1] += carry;
-	}
+    for (i = len1 - 1; i >= 0; i--)
+    {
+        n1 = num1[i] - '0';
+        carry = 0;
+        for (j = len2 - 1; j >= 0; j--)
+        {
+            n2 = num2[j] - '0';
+            sum = n1 * n2 + result[i + j + 1] + carry;
+            carry = sum / 10;
+            result[i + j + 1] = sum % 10;
+        }
+        result[i + j + 1] += carry;
+    }
 
-	i = 0;
-	while (i < len1 + len2 && result[i] == 0)
-		i++;
+    // Skip leading zeros
+    i = 0;
+    while (i < len1 + len2 && result[i] == 0)
+        i++;
 
-	if (i == len1 + len2)
-		_putchar('0');
-	else
-	{
-		for (; i < len1 + len2; i++)
-			_putchar(result[i] + '0');
-	}
-	_putchar('\n');
-	free(result);
+    if (i == len1 + len2)
+        _putchar('0');
+    else
+    {
+        for (; i < len1 + len2; i++)
+            _putchar(result[i] + '0');
+    }
+    _putchar('\n');
+    free(result);
 }
+
 
 /**
  * main - Entry point numbers
