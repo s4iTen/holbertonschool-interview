d2d628a204c3cbe48795fb60846d66e5aaf9a8bc
s4iTen <ychouk102@gmail.com>
2024-08-28 01:24:25 +0200

    this is the rotate function

--- rotate_2d_matrix/0-rotate_2d_matrix.py
+++ rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -15,11 +15,9 @@ def rotate_2d_matrix(matrix):
     """
     n = len(matrix)
 
-    # Transpose the matrix
     for i in range(n):
         for j in range(i, n):
             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
 
-    # Reverse each row of the transposed matrix
     for i in range(n):
         matrix[i].reverse()
