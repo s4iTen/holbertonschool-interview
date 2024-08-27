a0ace734135aec8040d79013677f1f58b91ba0c3
s4iTen <ychouk102@gmail.com>
2024-08-28 00:56:28 +0200

    this is the header file

---
+++ infinite_multiplication/holberton.h
@@ -0,0 +1,10 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+void _putchar(char c);
+void print_error(void);
+int is_digit(char c);
+int _strlen(char *s);
+void multiply(char *num1, char *num2);
+
+#endif
