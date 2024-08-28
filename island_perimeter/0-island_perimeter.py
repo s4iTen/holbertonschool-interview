2b62437ffec757c9f36b3eb2f2793ea95576ce89
s4iTen <ychouk102@gmail.com>
2024-08-28 16:16:11 +0200

    this is the last updates

--- island_perimeter/0-island_perimeter.py
+++ island_perimeter/0-island_perimeter.py
@@ -4,11 +4,11 @@ Function to calculate the perimeter of an island in a grid.
 """
 
 
+"""
+Calculate the perimeter of an island (1s) in a 2D grid, where 0s represent water.
+Returns 0 if the grid is empty or has no land cells.
+"""
 def island_perimeter(grid):
-    """
-    Calculate the perimeter of an island (1s) in a 2D grid, where 0s represent water.
-    Returns 0 if the grid is empty or has no land cells.
-    """
 
     if not grid:
         return 0
