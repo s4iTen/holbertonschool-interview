8d6941b04f3401ee0d7bc894abe8010c07ba8812
s4iTen <ychouk102@gmail.com>
2024-08-28 16:12:19 +0200

    this is the last updates

--- island_perimeter/0-island_perimeter.py
+++ island_perimeter/0-island_perimeter.py
@@ -22,8 +22,8 @@ def island_perimeter(grid):
         for j in range(n):
             if grid[i][j] == 1:
                 for x, y in [(0, 1), (1, 0), (-1, 0), (0, -1)]:
-                    α, β = i + x, j + y
-                    if α >= m or β >= n or α < 0 or β < 0 or grid[α][β] == 0:
+                    α, b = i + x, j + y
+                    if α >= m or b >= n or α < 0 or b < 0 or grid[α][b] == 0:
                         perimeter += 1
 
     return perimeter
