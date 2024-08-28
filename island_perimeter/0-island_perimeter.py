72b8edccbe4a89e2fd23405be9f504a631262b2c
s4iTen <ychouk102@gmail.com>
2024-08-28 02:03:47 +0200

    this is the last updates

---
+++ island_perimeter/0-island_perimeter.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+""" Function to find perimiter of an island """
+
+
+def island_perimeter(grid):
+    """
+    Input: List of list of integers
+    Returns: Integer
+    """
+    if not grid:
+        return 0
+
+    perimeter = 0
+    rows, cols = len(grid), len(grid[0])
+
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                perimeter += 4
+
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 1
+                if i < rows - 1 and grid[i + 1][j] == 1:
+                    perimeter -= 1
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 1
+                if j < cols - 1 and grid[i][j + 1] == 1:
+                    perimeter -= 1
+
+    return perimeter
